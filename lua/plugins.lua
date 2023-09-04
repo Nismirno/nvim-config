888a823cd69398dafc17a99a757e8f90cd894638
Nikita <nismirnov@localhost.localdomain>
2023-09-04 23:05:52 +0300

    Updated config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,25 +1,35 @@
-return require('packer').startup(function(use) 
+return require('packer').startup(function(use)
     use('wbthomason/packer.nvim')
 
     use{'junegunn/fzf', run = "fzf#install()"}
     use('junegunn/fzf.vim')
 
-    use('williamboman/mason.nvim')
-    use('williamboman/mason-lspconfig.nvim')
-
-    use('neovim/nvim-lspconfig')
-    use('simrat39/rust-tools.nvim')
-    use{ 
-        'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} } 
+    use{
+        'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} }
     }
     use {'morhetz/gruvbox', config = function() vim.cmd.colorscheme("gruvbox") end }
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
-    }   
+    }
     use('nvim-treesitter/playground')
     use('nvim-lua/plenary.nvim')
     use('ThePrimeagen/harpoon')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    }
 end)
